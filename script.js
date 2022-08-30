66a13aea2cd35da06b7b797ce61ddcef84feca03
Needhi Deshmukh <80128695+needhideshmukh@users.noreply.github.com>
2022-08-30 21:04:06 +0530

    files added

---
+++ script.js
@@ -0,0 +1,16 @@
+function dropFunction() {
+    document.getElementById("dropContent").classList.toggle("show");
+}
+
+// window.onclick = function(e){
+//     if(e.target.contains(".dropDown"){
+//         document.getElementById('#dropContent').onclick = function(e){
+//             if(e.target.contains('.dropDownContent')){
+//                 document.querySelector(".dropbtn").appendChild(".show");
+//             }
+//         }
+//     }
+//     else{
+// document.querySelector(".dropbtn").removeChild(".show");
+//     })
+// }
